cb71af2a3a247beb432fa37d28bf1f035ee84cef
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2026-07-02 16:35:12 -0700

    Added option to use MJD instead of JDs

--- src/microlensing_ralph/toolbox/output_tools.py
+++ src/microlensing_ralph/toolbox/output_tools.py
@@ -1,13 +1,15 @@
 import numpy as np
 
 from bokeh.plotting import figure, output_file, save
+from bokeh.models import HoverTool
 
 def plot_outlier_results(
         plot_location,
         plot_name,
         lc,
         outlier_results,
-        outlier_sequence
+        outlier_sequence,
+        to_mjd=False
 ):
     """
     Plot outlier results for a single light curve (`plot_name`)
@@ -30,6 +32,10 @@ def plot_outlier_results(
         :meth:`ralph.analyst.light_curve_analyst.LightCurveAnalyst.hampel_filter`.
     :type outlier_results: dict
 
+    :param to_mjd: If set to `True` the JDs will be changed to MJDs, to improve
+        readability.
+    :type to_mjd: bool, optional
+
     :param outlier_sequence: A list of dictionaries with found outlier sequences,
         see :meth:`ralph.analyst.light_curve_analyst.LightCurveAnalyst.vet_outliers`.
     :type outlier_sequence: dict
@@ -41,11 +47,18 @@ def plot_outlier_results(
 
     output_file(filename=plot_location, title=f"{plot_name}")
 
+    if to_mjd:
+        x_label="MJD"
+        time = lc[:,0] - 2400000.0
+    else:
+        x_label="JD"
+        time = lc[:,0]
+
     p = figure(title=f"{plot_name}",
-               x_axis_label="JD", y_axis_label="mag",
+               x_axis_label=x_label, y_axis_label="mag",
                width=1000, height=800)
 
-    p.varea(x=lc[:,0],
+    p.varea(x=time,
             y1=medians + thresholds,
             y2=medians - thresholds,
             fill_color="grey",
@@ -58,6 +71,10 @@ def plot_outlier_results(
     for sequence in outlier_sequence:
         t_start = sequence["t_start"]
         t_end = sequence["t_end"]
+
+        if to_mjd:
+            t_start = t_start - 2400000.0
+            t_end = t_end - 2400000.0
 
         p.harea(
             x1=t_start,
@@ -73,23 +90,30 @@ def plot_outlier_results(
             line_color="cornflowerblue",
         )
 
-    p.scatter(lc[:,0], lc[:,1],
+    p.scatter(time, lc[:,1],
               legend_label=f"Cleaned {plot_name}",
               marker="circle",
               color="#DB8C1D", size=8)
+
+    hover=HoverTool(tooltips=[("JD", "$x"), ("mag", "$y")])
+    p.add_tools(hover)
 
     # create the coordinates for the errorbars
     err_xs = []
     err_ys = []
 
-    for x, y, yerr in zip(lc[:,0], lc[:,1], lc[:,2]):
+    for x, y, yerr in zip(time, lc[:,1], lc[:,2]):
         err_xs.append((x, x))
         err_ys.append((y - yerr, y + yerr))
 
     # plot them
     p.multi_line(err_xs, err_ys, color='#DB8C1D')
 
-    p.scatter(lc[outliers,0], lc[outliers,1],
+    if to_mjd:
+        time_outliers = lc[outliers,0] - 2400000.0
+    else:
+        time_outliers = lc[outliers,0]
+    p.scatter(time_outliers, lc[outliers,1],
               legend_label=f"Outliers",
               marker="x",
               color="navy", size=8)
